5bfddc5fc765c3b397db92a6d1bac647a3ff3a11
Slava <slavapas13@gmail.com>
2014-04-11 08:28:31 +0300

    Smarter Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,7 +21,6 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to line_item_path(assigns(:line_item))
     assert_redirected_to cart_path(assigns(:line_item).cart)
   end
 
@@ -36,7 +35,7 @@ class LineItemsControllerTest < ActionController::TestCase
   end
 
   test "should update line_item" do
-    patch :update, id: @line_item, line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id }
+    patch :update, id: @line_item, line_item: { product_id: @line_item.product_id }
     assert_redirected_to line_item_path(assigns(:line_item))
   end
 
